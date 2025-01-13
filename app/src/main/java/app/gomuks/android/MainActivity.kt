6faf95ff1271851d8220720c30611391424c22e4
Virkkunen <vrkknn@vrkknn.net>
2025-01-13 13:01:05 +0100

    feat: transparent navbar and insets

--- app/src/main/java/app/gomuks/android/MainActivity.kt
+++ app/src/main/java/app/gomuks/android/MainActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.util.Base64
 import android.util.Log
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.compose.setContent
@@ -29,6 +30,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -124,6 +128,23 @@ class MainActivity : ComponentActivity() {
 
         session.promptDelegate = BasicGeckoViewPrompt(this)
         session.navigationDelegate = navigation
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+            )
+
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, insets ->
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                v.paddingLeft,
+                systemBarsInsets.top,
+                v.paddingRight,
+                v.paddingBottom
+            )
+            insets
+        }
 
         val sessWebExtController = session.webExtensionController
         runtime.webExtensionController
