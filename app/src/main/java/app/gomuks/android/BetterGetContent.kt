2a02822eb0a1bb6e4ae8c995484cffb61bb5b1b8
Tulir Asokan <tulir@maunium.net>
2025-11-29 17:39:45 +0200

    Remove unused import

--- app/src/main/java/app/gomuks/android/BetterGetContent.kt
+++ app/src/main/java/app/gomuks/android/BetterGetContent.kt
@@ -7,7 +7,6 @@ import android.content.Intent
 import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
-import androidx.activity.ComponentActivity
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.core.content.FileProvider
 import androidx.core.net.toUri
